418e7e43cb9e4971cd0be6d39df13885ee7cca4f
FIRMAN PANJI <fpu2468@gmail.com>
2024-07-03 00:00:17 +0700

    init Chapter 7

---
+++ F-BJV24001115-synergy7-fir-binfud-ch7/F-BJV24001115-synergy7-fir-binfud-ch7/src/main/java/com/example/FBJV24001115synergy7firbinfudch6/service/OtpServiceImpl.java
@@ -0,0 +1,15 @@
+package com.example.FBJV24001115synergy7firbinfudch6.service;
+
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+
+@Service
+public class OtpServiceImpl {
+    private final SecureRandom random = new SecureRandom();
+
+    public String generateOtp() {
+        int otp = 100000 + random.nextInt(900000);
+        return String.valueOf(otp);
+    }
+}
---
+++ F-BJV24001115-synergy7-fir-binfud-ch7/F-BJV24001115-synergy7-fir-binfud-ch7/src/main/java/com/example/FBJV24001115synergy7firbinfudch6/service/ProductServiceImpl.java
@@ -0,0 +1,64 @@
+package com.example.FBJV24001115synergy7firbinfudch6.service;
+
+import com.example.FBJV24001115synergy7firbinfudch6.model.entity.Merchant;
+import com.example.FBJV24001115synergy7firbinfudch6.model.entity.Product;
+import com.example.FBJV24001115synergy7firbinfudch6.repository.MerchantRepository;
+import com.example.FBJV24001115synergy7firbinfudch6.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private MerchantRepository merchantRepository;
+
+//    @Override
+//    public Product addProduct(Product product) {
+//        return productRepository.save(product);
+//    }
+
+    @Override
+    public Product addProduct(Product product) {
+        UUID merchantId = product.getMerchant().getId();
+        Merchant merchant = merchantRepository.findById(merchantId)
+                .orElseThrow(() -> new IllegalArgumentException("Merchant not found"));
+        product.setMerchant(merchant);
+
+        if (product.getPrice() == null) {
+            throw new IllegalArgumentException("Product price cannot be null");
+        }
+
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product updateProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public void deleteProduct(UUID productId) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new IllegalArgumentException("Product not found"));
+        product.setDeletedDate(new Date());
+        productRepository.save(product);
+    }
+
+    @Override
+    public Product getProductById(UUID productId) {
+        return productRepository.findById(productId)
+                .orElseThrow(() -> new IllegalArgumentException("Product not found"));
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+}
